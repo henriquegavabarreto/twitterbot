c1f5cf3894d05979d2d15ec6bd79c042ce0116e6
henrique <henriquegavabarreto@gmail.com>
2019-06-26 20:36:05 -0400

    add dotenv and twit, get keys

---
+++ index.js
@@ -0,0 +1,13 @@
+require('dotenv').config()
+var Twit = require('twit')
+
+var T = new Twit({
+  consumer_key: process.env.API_KEY,
+  consumer_secret: process.env.API_SECRET,
+  access_token: process.env.ACCESS_TOKEN,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+  strictSSL:            true,     // optional - requires SSL certificates to be valid.
+})
+
+console.log('Starting ParaPara News')
