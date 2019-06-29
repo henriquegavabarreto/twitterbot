c2946e236191dd1b3e493bcc5a9d032178884b02
henrique <henriquegavabarreto@gmail.com>
2019-06-29 19:52:50 -0400

    add tweetMsg, add tweetRandomVideo, add tweetNewVideos, installed standard to lint and schedule to schedule tweets

--- index.js
+++ index.js
@@ -3,74 +3,149 @@ const creds = require('./client_secret.json')
 const GoogleSpreadsheet = require('google-spreadsheet')
 const { promisify } = require('util')
 const YouTube = require('simple-youtube-api')
-// var Twit = require('twit')
+var schedule = require('node-schedule')
+var Twit = require('twit')
 
 const youtube = new YouTube(process.env.YOUTUBE_API_KEY)
 
-// var T = new Twit({
-//   consumer_key: process.env.API_KEY,
-//   consumer_secret: process.env.API_SECRET,
-//   access_token: process.env.ACCESS_TOKEN,
-//   access_token_secret: process.env.ACCESS_TOKEN_SECRET,
-//   timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
-//   strictSSL:            true,     // optional - requires SSL certificates to be valid.
-// })
+var T = new Twit({
+  consumer_key: process.env.API_KEY,
+  consumer_secret: process.env.API_SECRET,
+  access_token: process.env.ACCESS_TOKEN,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+  timeout_ms: 60 * 1000, // optional HTTP request timeout to apply to all requests.
+  strictSSL: true // optional - requires SSL certificates to be valid.
+})
 
-console.log('Starting ParaPara News')
+let doc = new GoogleSpreadsheet('14qlEWwnIoq1aFup3gJqgju1CKMUr35h-3tH_GxcYSzU')
 
 let ytRegExp = /(parapara|eurobeat|techpara|trapara|trance|テクパラ|パラパラ|ユーロ|テクノ|ユーロビート|トラパラ|トランス)/gi
+
+console.log('Starting ParaPara News')
 
 function validateVideo (title, description) {
   return (ytRegExp.test(title) || ytRegExp.test(description))
 }
 
-let doc = new GoogleSpreadsheet('14qlEWwnIoq1aFup3gJqgju1CKMUr35h-3tH_GxcYSzU')
-
-// returns a promise with the rows of the active channels spreadsheet
-function getCurrentActiveChannels() {
-  return promisify(doc.useServiceAccountAuth)(creds).then(() => {
-    return promisify(doc.getInfo)().then((info)=> {
+// removes all data from active spreadsheet and add all active channels names and id
+function setActiveChannels () {
+  promisify(doc.useServiceAccountAuth)(creds).then(() => {
+    promisify(doc.getInfo)().then(info => {
+      let allChannels = info.worksheets[0]
       let activeChannels = info.worksheets[1]
-      return promisify(activeChannels.getRows)({ offset: 1 }).then(rows => {
+      // clear all active sheet
+      console.log('Clear Active worksheet')
+      promisify(activeChannels.clear)().then(() => {
+        console.log('Cleared')
+        // set the header
+        promisify(activeChannels.setHeaderRow)(['channelname', 'channelid']).then(() => {
+          console.log('Set header')
+          // check for active channels
+          promisify(allChannels.getRows)({ offset: 1 }).then(rows => {
+            rows.forEach(row => {
+              checkActivity(row.channelid).then(channelInfo => {
+                console.log(`Adding ${channelInfo.channelname} to the list`)
+                // add channel to the document if active
+                promisify(activeChannels.addRow)(channelInfo)
+              }).catch(err => console.log(err))
+            })
+          }).catch(err => console.log(err))
+        }).catch(err => console.log(err))
+      }).catch(err => console.log(err))
+    }).catch(err => console.log(err))
+  }).catch(err => console.log(err))
+}
+
+function checkActivity (channelId) {
+  return new Promise(function (resolve, reject) {
+    youtube.getChannelByID(channelId, { part: 'contentDetails' }).then(channel => {
+      let lastYear = new Date()
+      lastYear.setFullYear(lastYear.getFullYear() - 1)
+      if (channel) {
+      // get channel uploads
+        youtube.getPlaylistByID(channel.relatedPlaylists.uploads).then(playlist => {
+          if (playlist) {
+            // get latest 3 videos of the channel
+            playlist.getVideos(3, { part: 'snippet' }).then(videos => {
+              let active = false
+              let channelInfo = {}
+              for (let video of videos) {
+                channelInfo = {
+                  channelname: video.channel.title,
+                  channelid: video.channel.id
+                }
+                if (new Date(video.publishedAt) > lastYear) {
+                  if (validateVideo(video.title, video.description)) {
+                    active = true
+                  }
+                }
+              }
+              if (active) {
+                resolve(channelInfo)
+              } else {
+                reject(new Error(`${channelInfo.channelname} has no relevant videos for more than year`))
+              }
+            }).catch(error => reject(error))
+          } else {
+            reject(new Error('Playlist not found'))
+          }
+        }).catch(error => reject(error))
+      } else {
+        reject(new Error('Channel not found'))
+      }
+    }).catch(error => reject(error))
+  })
+}
+
+// returns a promise with the rows of the selected spreadsheet - 0 for all and 1 for active
+function getSpreadsheet (n) {
+  return promisify(doc.useServiceAccountAuth)(creds).then(() => {
+    return promisify(doc.getInfo)().then(info => {
+      let sheet = info.worksheets[n]
+      return promisify(sheet.getRows)({ offset: 1 }).then(rows => {
         return Promise.resolve(rows)
       }).catch(err => console.log(err))
     }).catch(err => console.log(err))
   }).catch(err => console.log(err))
 }
 
-getCurrentActiveChannels().then(rows => {
-  let promiseArray = []
-  rows.forEach(row => {
-    promiseArray.push(getNewVideosFromChannel(row.channelid))
-  })
-  Promise.all(promiseArray).then(response => {
-    let newVideos = []
-    // ignore non video responses
-    response.forEach(video => {
-      if (video) {
-        newVideos.push(video)
-      }
+// log new videos in the last 24 hours
+function getAllNewVideos () {
+  return getSpreadsheet(1).then(rows => {
+    let promiseArray = []
+    rows.forEach(row => {
+      promiseArray.push(getNewVideosFromChannel(row.channelid))
     })
-    if (newVideos.length > 0) {
-      console.log(`We got ${newVideos.length} new videos since yesterday!`)
-      newVideos.forEach(video => {
-        // deal with new videos information
-        console.log(`${video.channelName} uploaded ${video.title}!\nCheck it out on youtube!\nhttps://youtu.be/${video.id}`)
+    return Promise.all(promiseArray).then(response => {
+      let newVideos = []
+      // ignore non video responses
+      response.forEach(video => {
+        if (video) {
+          newVideos.push(video)
+        }
       })
-    } else {
-      // deal with having no videos
-      console.log('No New Videos Today')
-    }
-  }).catch(err => console.log(err))
-})
+      if (newVideos.length > 0) {
+        console.log(`We got ${newVideos.length} new videos in the last 24 hours!`)
+        newVideos.forEach(video => {
+          // deal with new videos information
+          console.log(`${video.channelName} uploaded ${video.title}!\nCheck it out on youtube!\nhttps://youtu.be/${video.id}`)
+        })
+        return newVideos
+      } else {
+        // deal with having no videos
+        console.log('No New Videos Today')
+      }
+    }).catch(err => console.log(err))
+  })
+}
 
 function getNewVideosFromChannel (channelId) {
-  return new Promise(function(resolve, reject) {
+  return new Promise(function (resolve, reject) {
     youtube.getChannelByID(channelId, { part: 'contentDetails' }).then(channel => {
       let yesterday = new Date()
       yesterday.setDate(yesterday.getDate() - 1)
       if (channel) {
-       // get channel uploads
+        // get channel uploads
         youtube.getPlaylistByID(channel.relatedPlaylists.uploads).then(playlist => {
           if (playlist) {
             // get latest 2 videos of the channel
@@ -81,8 +156,8 @@ function getNewVideosFromChannel (channelId) {
                   if (new Date(video.publishedAt) >= yesterday) {
                     if (validateVideo(video.title, video.description)) {
                       let videoInfo = {
-                        id: video.id,
-                        title: video.title,
+                        videoId: video.id,
+                        videoTitle: video.title,
                         channelName: video.channel.title
                       }
                       resolve(videoInfo)
@@ -99,11 +174,11 @@ function getNewVideosFromChannel (channelId) {
               })
             }).catch(error => reject(error))
           } else {
-            reject('playlist not found :(')
+            reject(new Error('Playlist not found'))
           }
-        }).catch(error => reject(error));
-     } else {
-        reject('channel not found :(')
+        }).catch(error => reject(error))
+      } else {
+        reject(new Error('Channel not found'))
       }
     }).catch(error => reject(error))
   })
@@ -111,41 +186,36 @@ function getNewVideosFromChannel (channelId) {
 
 // returns a random channel from the spreadsheet
 function getRandomChannel () {
-  return promisify(doc.useServiceAccountAuth)(creds).then(() => {
-    return promisify(doc.getInfo)().then((info)=> {
-      let channels = info.worksheets[0]
-      return promisify(channels.getRows)({ offset: 1 }).then(rows => {
-        let randomNumber = getRandomNumber(rows.length)
-        return rows[randomNumber].channelid
-      }).catch(err => console.log(err))
-    }).catch(err => console.log(err))
+  getSpreadsheet(0).then(rows => {
+    let randomNumber = getRandomNumber(rows.length)
+    return rows[randomNumber].channelid
   }).catch(err => console.log(err))
 }
 
 // returns a random video id and title
 function getRandomVideo (channelId) {
-  return new Promise(function(resolve, reject) {
+  return new Promise(function (resolve, reject) {
     youtube.getChannelByID(channelId, { part: 'contentDetails' }).then(channel => {
       if (channel) {
-       // get channel uploads
+        // get channel uploads
         youtube.getPlaylistByID(channel.relatedPlaylists.uploads).then(playlist => {
           if (playlist) {
             // get all videos of the channel
             playlist.getVideos(undefined, { part: 'snippet' }).then(videos => {
               let selectedVideo = selectVideo(videos, getRandomNumber(videos.length))
               let info = {
-                id: selectedVideo.id,
+                videoId: selectedVideo.id,
                 videoTitle: selectedVideo.title,
-                channelTitle: selectedVideo.channel.title
+                channelName: selectedVideo.channel.title
               }
               resolve(info)
             }).catch(error => reject(error))
           } else {
-            reject('playlist not found :(')
+            reject(new Error('Playlist not found'))
           }
-        }).catch(error => reject(error));
-     } else {
-        reject('channel not found :(')
+        }).catch(error => reject(error))
+      } else {
+        reject(new Error('Channel not found'))
       }
     }).catch(error => reject(error))
   })
@@ -153,7 +223,7 @@ function getRandomVideo (channelId) {
 
 // returns a valid video
 function selectVideo (videos, randomNumber) {
-  if(validateVideo(videos[randomNumber].title, videos[randomNumber].description)) {
+  if (validateVideo(videos[randomNumber].title, videos[randomNumber].description)) {
     return videos[randomNumber]
   } else {
     videos.splice(randomNumber, 1)
@@ -166,17 +236,48 @@ function getRandomNumber (max) {
   return Math.floor(Math.random() * max)
 }
 
-function getRandomVideoInfo () {
+function tweetMsg (msg) {
+  let tweet = { status: msg }
+  T.post('statuses/update', tweet, function (err, data, response) {
+    if (err) {
+      console.log(err)
+    } else {
+      console.log('Tweeted!')
+    }
+  })
+}
+
+function tweetRandomVideo () {
   return getRandomChannel().then(channelId => {
     return getRandomVideo(channelId).then(videoInfo => {
-      console.log(videoInfo)
+      tweetMsg(`It's time for a random video! This one is from ${videoInfo.channelName}!\n\n${videoInfo.videoTitle}\n\nhttps://youtu.be/${videoInfo.videoId}\n\n#parapara #パラパラ`)
     }).catch(err => console.log(err))
   }).catch(err => console.log(err))
 }
 
-// getRandomVideoInfo()
-// setInterval(getRandomVideoInfo, 20000)
+function tweetNewVideos () {
+  getAllNewVideos().then(videos => {
+    if (videos.length === 0) {
+      tweetRandomVideo()
+    } else if (videos.length === 1) {
+      tweetMsg(`This a video posted on the last 24h from ${videos[0].channelName}!\n\n${videos[0].videoTitle}\n\nhttps://youtu.be/${videos[0].videoId}\n\n#parapara #パラパラ`)
+    } else {
+      let timeOut = 1000 * 60 * 60 * 12 / (videos.length - 1)
+      // post new videos in the next 12 hours
+      videos.forEach((video, index) => {
+        setTimeout((video) => {
+          tweetMsg(`[${index + 1}/${videos.length}]\n\nThis one of the videos posted on the last 24h! From ${video.channelName}!\n\n${video.videoTitle}\n\nhttps://youtu.be/${video.videoId}\n\n#parapara #パラパラ`)
+        }, index * timeOut)
+      })
+    }
+  }).catch(err => console.log(err))
+}
 
-// T.get('search/tweets', { q: 'sefdeluxe since:2011-07-11', count: 2 }, function(err, data, response) {
-//   console.log(data)
-// })
+// tweet a random video every day at 9pm
+var j1 = schedule.scheduleJob('0 21 * * *', tweetRandomVideo)
+
+var j2 = schedule.scheduleJob('0 0 1 * *', setActiveChannels)
+
+var j3 = schedule.scheduleJob('0 8 * * *', tweetNewVideos)
+
+// TODO: retweet every tweet that has parapara and youtu.be, but ignoring users with 'parapara' - maybe check if it is cool before retweeting (?)
